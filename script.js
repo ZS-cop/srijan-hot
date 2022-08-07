a3791dbb56b605a461edff73e2036a66b4397ea7
Zodiac Software <86096723+ZS-cop@users.noreply.github.com>
2022-08-07 14:45:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let type = new Typed("#type" , {
+         strings: ["Srijan" , "HOT" , "CRUSH OF EVERY GIRL" , "YOUR DAD"] , 
+         typeSpeed:150 , 
+         backSpeed:150 ,
+         loop: true
+});
+
+let about = new Typed("#about" , {
+         strings: ["Srijan is the topper of grade - IX , Cracked IIT , HACKED GOOGLE WITH HIS SKILL ,HAVE 30 GF , Master in quantum phy"] , 
+         typeSpeed:150 , 
+         loop: true
+});
